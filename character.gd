62c8b50c7e9423791ce01528536f4acea8b3d28c
ggammoh <gavingoomoh@gmail.com>
2023-11-04 17:42:48 -0400

    Floor Collision

--- character.gd
+++ character.gd
@@ -1,13 +1,16 @@
 extends CharacterBody3D
 
-
-const SPEED = 5.0
+const SPEED = 4.0
 const JUMP_VELOCITY = 4.5
-
+const FRICTION = 25
+const HORIZONTAL_ACCELERATION = 30
+const MAX_SPEED=5
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 
 @onready var camera = $Camera3D
+
+
 
 func _ready():
 	Input.mouse_mode=Input.MOUSE_MODE_CAPTURED
@@ -32,18 +35,35 @@ func _physics_process(delta):
 
 	# Handle Jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor() and Input.mouse_mode==Input.MOUSE_MODE_CAPTURED:
-		velocity.y = JUMP_VELOCITY
+		velocity.y += JUMP_VELOCITY
+		$AnimationTree.set("parameters/conditions/Jump", true)
+	else:
+		$AnimationTree.set("parameters/conditions/Jump", false)
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var input_dir = Input.get_vector("move_left", "move_right", "move_forward", "move_backward")
+	var input_dir = Vector3.ZERO
+	var movetoward = Vector3.ZERO
+	input_dir.x = Input.get_vector("move_left", "move_right", "move_forward", "move_backward").x
+	input_dir.y = Input.get_vector("move_left", "move_right", "move_forward", "move_backward").y
+	input_dir=input_dir.normalized()
+
 	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
-	if direction and Input.mouse_mode==Input.MOUSE_MODE_CAPTURED:
-		velocity.x = direction.x * SPEED
-		velocity.z = direction.z * SPEED
+	if Input.is_action_pressed("shift_run"):
+		direction *= (SPEED + 3)
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		velocity.z = move_toward(velocity.z, 0, SPEED)
+		direction *= SPEED
+	velocity.x = move_toward(velocity.x,direction.x, HORIZONTAL_ACCELERATION * delta)
+	velocity.z = move_toward(velocity.z,direction.z, HORIZONTAL_ACCELERATION * delta)
+
+	var angle=5
+	#rotation_degrees=Vector3(input_dir.normalized().y*angle,rotation_degrees.y,-input_dir.normalized().x*angle)
+	var t = delta * 6
+	if Input.mouse_mode==Input.MOUSE_MODE_CAPTURED: 
+		rotation_degrees=rotation_degrees.lerp(Vector3(input_dir.normalized().y*angle,rotation_degrees.y,-input_dir.normalized().x*angle),t)
+	
+	$AnimationTree.set("parameters/BlendSpace2D/blend_position", Vector2(velocity.x, -velocity.z) / (3*SPEED))
+	$AnimationTree.set("parameters/conditions/Grounded", is_on_floor())
 	
 	move_and_slide()
 	force_update_transform()
