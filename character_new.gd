a3fe6704127d287e1e95745b47b4482cd5e3312b
ggammoh <gavingoomoh@gmail.com>
2023-11-04 22:23:43 -0400

    Character Grav

--- character_new.gd
+++ character_new.gd
@@ -1,7 +1,7 @@
 extends CharacterBody3D
 
 const SPEED = 8.0
-const JUMP_VELOCITY = 9
+const JUMP_VELOCITY = 14
 const FRICTION = 25
 const HORIZONTAL_ACCELERATION = 30
 const MAX_SPEED=5
@@ -61,7 +61,7 @@ func _physics_process(delta):
 	input_dir=input_dir.normalized()
 	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if Input.is_action_pressed("shift_run"):
-		direction *= (SPEED + 3)
+		direction *= (SPEED + 10)
 	else:
 		direction *= SPEED
 	
