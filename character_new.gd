3420b5cbba63f226630d15785f2e0f16e0c52732
Andrew Partin <drew.partin25@gmail.com>
2023-11-04 21:50:20 -0400

    butt

--- character_new.gd
+++ character_new.gd
@@ -1,15 +1,19 @@
 extends CharacterBody3D
 
-const SPEED = 5.0
-const JUMP_VELOCITY = 4.5
+const SPEED = 8.0
+const JUMP_VELOCITY = 9
 const FRICTION = 25
 const HORIZONTAL_ACCELERATION = 30
 const MAX_SPEED=5
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 
-@onready var camera = $Camera3D
+var cheese = load("res://cheese_2.tscn")
+var c_instance
 
+@onready var camera = $Camera3D
+@onready var raytrace = $Camera3D/RayCast3D
+@onready var cheese_area = $Camera3D/cheese_area
 
 
 func _ready():
@@ -36,6 +40,17 @@ func _physics_process(delta):
 	# Handle Jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor() and Input.mouse_mode==Input.MOUSE_MODE_CAPTURED:
 		velocity.y += JUMP_VELOCITY
+		$AnimationTree.set("parameters/conditions/Jump", true)
+	else:
+		$AnimationTree.set("parameters/conditions/Jump", false)
+				
+	if Input.is_action_just_pressed("throw_cheese"):
+		c_instance = cheese.instantiate()
+		c_instance.position = cheese_area.global_position
+		c_instance.linear_velocity = -8*camera.get_global_transform().basis.z
+		c_instance.angular_velocity = Vector3(randf_range(-10, 10), randf_range(-10, 10), randf_range(-10, 10))
+		get_parent().add_child(c_instance)
+		
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
@@ -45,7 +60,11 @@ func _physics_process(delta):
 	input_dir.y = Input.get_vector("move_left", "move_right", "move_forward", "move_backward").y
 	input_dir=input_dir.normalized()
 	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
-	direction *= SPEED
+	if Input.is_action_pressed("shift_run"):
+		direction *= (SPEED + 3)
+	else:
+		direction *= SPEED
+	
 	velocity.x = move_toward(velocity.x,direction.x, HORIZONTAL_ACCELERATION * delta)
 	velocity.z = move_toward(velocity.z,direction.z, HORIZONTAL_ACCELERATION * delta)
 
@@ -54,6 +73,9 @@ func _physics_process(delta):
 	var t = delta * 6
 	if Input.mouse_mode==Input.MOUSE_MODE_CAPTURED: 
 		rotation_degrees=rotation_degrees.lerp(Vector3(input_dir.normalized().y*angle,rotation_degrees.y,-input_dir.normalized().x*angle),t)
+	
+	$AnimationTree.set("parameters/BlendSpace2D/blend_position", Vector2(velocity.x, -velocity.z) / (3*SPEED))
+	$AnimationTree.set("parameters/conditions/Grounded", is_on_floor())
 	
 	move_and_slide()
 	force_update_transform()
